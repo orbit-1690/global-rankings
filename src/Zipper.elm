266e8d441e1719cbf2914f36b65ab3a887c15c5e
Hadar <nahshon.hadar@gmail.com>
2019-12-27 00:23:23 +0200

    a few small improvements

--- src/Zipper.elm
+++ src/Zipper.elm
@@ -12,7 +12,7 @@ module Zipper exposing
     , moveForwardWhile
     )
 
-import List exposing (append, head, isEmpty, tail)
+import List exposing (reverse)
 import List.Extra exposing (init, last)
 import Maybe exposing (andThen, withDefault)
 
@@ -24,39 +24,36 @@ type alias Zipper a =
     }
 
 
-makeZipper : List a -> a -> List a -> Maybe (Zipper a)
+makeZipper : List a -> a -> List a -> Zipper a
 makeZipper previous current next =
-    if Just current == Nothing then
-        Nothing
-
-    else
-        Just { previous = previous, current = current, next = next }
+    { previous = previous, current = current, next = next }
 
 
 moveForward : Zipper a -> Maybe (Zipper a)
 moveForward zipper =
-    if isEmpty zipper.previous then
-        Nothing
+    case init zipper.previous of
+        Nothing ->
+            Nothing
 
-    else
-        Just <|
-            { previous = withDefault [] <| init zipper.previous
-            , current = withDefault zipper.current <| last zipper.previous
-            , next = zipper.current :: zipper.next
-            }
+        Just choppedPrevious ->
+            Just <|
+                { previous = choppedPrevious
+                , current = withDefault zipper.current <| last zipper.previous
+                , next = zipper.current :: zipper.next
+                }
 
 
 moveBackwards : Zipper a -> Maybe (Zipper a)
 moveBackwards zipper =
-    if isEmpty zipper.next then
-        Nothing
+    moveForward << flipZipp <| zipper
 
-    else
-        Just <|
-            { previous = append zipper.previous [ zipper.current ]
-            , current = withDefault zipper.current <| head zipper.next
-            , next = withDefault [] <| tail zipper.next
-            }
+
+flipZipp : Zipper a -> Zipper a
+flipZipp zipper =
+    { previous = reverse zipper.next
+    , current = zipper.current
+    , next = reverse zipper.previous
+    }
 
 
 moveForwardTo : a -> Zipper a -> Maybe (Zipper a)
